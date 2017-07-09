abad5663a06e54c4eee1744aff48e291caea32e2
Torres63 <joyoa.t2013@gmail.com>
2017-07-08 19:37:27 -0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,56 @@
+
+class Variables {
+	/**
+	 * short - numeros cortos
+	 * int -numeros enteros
+	 * long-numeros largos
+	 * float - numeros flotantes
+	 *
+	 * double - numeros dobles
+	 * byte - se maneja en bytes
+	 * char - caracteres
+	 * boolean - booleanos,  verdadero-falso
+	 * String - cadena de texto
+	 *
+	 * @param args
+	 */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float hola2;
+		hola2 = 5;
+		double hola;
+		hola = hola2 / 2;
+                int hola5;
+                hola5 = (int) (hola2 * hola);
+                int hola6;
+                int hola7;
+                int hola8;
+                hola6 = 3;
+                hola7 = 4;
+                hola8 = (int) hola7 * hola6;
+
+		
+		int hola3;
+		hola3 = 5;
+		int hola4;
+		hola4 = hola3 / 2;
+
+
+	 System.out.println("hola3: "+hola3);
+	 System.out.println("el resultado: "+hola4);
+	 System.out.println("hola2 :"+hola2);
+	 System.out.println("el resultado :"+hola);
+   System.out.println("hola5 es:"+hola5);
+   System.out.println("hola8 es:"+hola8);
+
+
+
+
+
+	}
+
+}
