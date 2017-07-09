736e19f53c0993a47b424bd10290d481ac222ef9
Torres63 <joyoa.t2013@gmail.com>
2017-07-08 21:56:55 -0600

    Create Calculadora Par-Impar suculenta cx.java

---
+++ Calculadora Par-Impar suculenta cx.java
@@ -0,0 +1,26 @@
+package javaapplication2;
+import java.util.Scanner;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+   
+        Scanner sc=new Scanner(System.in);
+        System.out.println("tu numero");
+        int numero=sc.nextInt();
+        if(numero%2==0){
+            
+            System.out.println("Numero es par");
+          
+        }
+        
+        else{
+            if(numero%2==0){
+               
+                System.out.println(numero es par);
+            }
+            else{
+                System.out.println("tu numero no es par");
+            }
+            
+        }
