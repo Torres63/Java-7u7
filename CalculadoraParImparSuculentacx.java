940bed15aa09a710ab70bc57899881cfbe99ba2a
Torres63 <joyoa.t2013@gmail.com>
2017-07-09 09:47:36 -0600

    Update CalculadoraParImparSuculentacx.java

--- CalculadoraParImparSuculentacx.java
+++ CalculadoraParImparSuculentacx.java
@@ -1,11 +1,11 @@
 package CalculadoraParImparSuculentacx;
+
 import java.util.Scanner;
 
-public class JavaApplication2 {
+public class CalculadoraParImparSuculentacx {
 
     public static void main(String[] args) {
-   
-        Scanner sc=new Scanner(System.in);
+          Scanner sc=new Scanner(System.in);
         System.out.println("tu numero");
         int numero=sc.nextInt();
         if(numero%2==0){
@@ -15,12 +15,8 @@ public class JavaApplication2 {
         }
         
         else{
-            if(numero%2==0){
-               
-                System.out.println(numero es par);
-            }
-            else{
-                System.out.println("tu numero no es par");
-            }
-            
+            System.out.println("es impar");
         }
+    }
+    
+}
