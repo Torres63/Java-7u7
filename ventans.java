14eca05c82b24e67d6df48ae242c31db60fd98ed
Torres63 <joyoa.t2013@gmail.com>
2017-07-16 22:11:21 -0600

    Create ventans.java

---
+++ ventans.java
@@ -0,0 +1,28 @@
+package ventans;
+
+import javax.swing.JFrame;
+
+public class ventana extends JFrame {	
+	public ventana() {
+		this.setSize(500, 500);
+		setTitle("ventana");
+		iniciarComponentes();
+	}
+	
+	
+	private void iniciarComponentes() {
+		addWindowListener(new java.awt.event.
+		
+			
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ventana x = new ventana();
+		x.setVisible(true);
+		
+
+	}
+
+}
