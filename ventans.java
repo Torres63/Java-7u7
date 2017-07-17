9d28494e0fae68ad8948a2a7ffd58537c1ad8c88
Torres63 <joyoa.t2013@gmail.com>
2017-07-16 22:33:40 -0600

    Update ventans.java

--- ventans.java
+++ ventans.java
@@ -1,27 +1,32 @@
 package ventans;
-
 import javax.swing.JFrame;
 
 public class ventana extends JFrame {	
 	public ventana() {
 		this.setSize(500, 500);
-		setTitle("ventana");
-		iniciarComponentes();
-	}
-	
-	
-	private void iniciarComponentes() {
-		addWindowListener(new java.awt.event.
+                setTitle("Hola");
+                iniciarComponentes();
 		
-			
-	}
+	
+
+  	}
+        private void iniciarComponentes(){
+            addWindowListener(new java.awt.event.WindowAdapter() {
+                   
+                
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent Evento){
+                       System.exit(0);
+                    }
+                
+});
+        }
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ventana x = new ventana();
 		x.setVisible(true);
-		
 
 	}
 
